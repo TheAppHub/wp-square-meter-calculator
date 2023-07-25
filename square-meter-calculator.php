d82f48cc0d092354b06809feb6fcd34b0fd7c5b2
Christoph <christoph@hindsihgt.com.au>
2023-07-26 09:32:13 +1000

    init

---
+++ square-meter-calculator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Square Meter Calculator
+ * Plugin URI:      https://github.com/TheAppHub/wp-square-meter-calculator
+ * Description:     Calculator to convert square meters to product package and vice versa.
+ * Author:          Christoph - The App Hub
+ * Author URI:      https://theapphub.com.au
+ * Text Domain:     square-meter-calculator
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Square_Meter_Calculator
+ */
+
+// Your code starts here.
